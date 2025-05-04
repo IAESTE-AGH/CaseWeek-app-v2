3b35e7a733d57d7adf3118c6bd010888a5e29df9
Bartek Sosin <bscode03@gmail.com>
2025-05-04 15:43:46 +0200

    Chnge routoing structure, type companies,  clean files structure in components

---
+++ src/pages/landing/partnersCompanies/PartnersCompanies.tsx
@@ -0,0 +1,32 @@
+import s from "./PartnersCompanies.module.scss";
+import temporaryCompaniesLogo from "./PSRP_logo.png";
+import { PartnersPageProps } from "./PartnersCompanies.type";
+
+const PartnersCompanies: React.FC<PartnersPageProps> = ({
+  companies,
+  title,
+}) => {
+  return (
+    <section className={s.partnersSection}>
+      <h1 className={s.title}>{title}</h1>
+      <ul className={s.partnersGrid}>
+        {companies.map((company) => (
+          <li key={company.id} className={s.partner}>
+            <div className={s.partnerWrapper}>
+              <a
+                href={company.websiteUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={temporaryCompaniesLogo} alt={company.name} />
+              </a>
+              {/*  <p className={s.partnerStatus}>{t("partners.honoraryStatus")}</p> */}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default PartnersCompanies;
---
+++ src/types/company.ts
@@ -0,0 +1,9 @@
+export type Company = {
+  id: string;
+  name: string;
+  logoUrl: string;
+  websiteUrl: string;
+  shortDescription: string;
+  longDescription: string;
+  displayPriority: number;
+};
