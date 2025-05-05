e9047989193e8323b7fb5faec0877850a68d2ea9
Bartek Sosin <bscode03@gmail.com>
2025-05-05 17:27:46 +0200

    add footer

---
+++ src/components/templates/footer/Footer.const.ts
@@ -0,0 +1,33 @@
+import fbIcon from "../../../assets/icons/facebook_icon.svg";
+import ytIcon from "../../../assets/icons/youtube_icon.svg";
+import igIcon from "../../../assets/icons/instagram_icon.svg";
+import limkedinIcon from "../../../assets/icons/linkedin_icon.svg";
+import iaesteLogo from "../../../assets/img/logo_circle.png";
+
+export const FOOTER_ICONS = [
+  {
+    icon: fbIcon,
+    link: "https://www.facebook.com/IaestePoland/",
+    alt: "facebook-logo",
+  },
+  {
+    icon: ytIcon,
+    link: "https://www.youtube.com/channel/UC4TzR5wg4WlwnupQMztroUQ",
+    alt: "youtube-logo",
+  },
+  {
+    icon: iaesteLogo,
+    link: "https://www.iaeste.pl/",
+    alt: "iaeste-logo",
+  },
+  {
+    icon: igIcon,
+    link: "https://www.instagram.com/iaeste_poland/",
+    alt: "instagram-logo",
+  },
+  {
+    icon: limkedinIcon,
+    link: "https://www.linkedin.com/company/iaeste-poland/",
+    alt: "linkedin-logo",
+  },
+];
