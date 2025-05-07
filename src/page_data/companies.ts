517087a62aa8cfbc0e724e1ff351e2900f430a9a
ShakingDonut <kacper.siembab@gmail.com>
2025-05-07 13:24:49 +0200

    add companies and workshops data

---
+++ src/page_data/companies.ts
@@ -0,0 +1,65 @@
+import { Company } from "@/types/company";
+
+export const MOCK_COMPANIES: Company[] = [
+  {
+    id: "c1",
+    name: "Przedsiębiorstwo Robót Inżynieryjnych INKOP Sp. z o.o.",
+    logoUrl: "https://logos.gpcdn.pl/loga-firm/20189608/2c580000-43a8-f403-89e0-08d91a0e1193_280x280.png",
+    websiteUrl: "https://www.inkop.pl/",
+  },
+
+  {
+    id: "c2",
+    name: "ORLEN Termika S.A.",
+    logoUrl: "https://biznesalert.pl/wp-content/uploads/2025/01/Zdjecie-WhatsApp-2025-01-15-o-12.22.26_5dd7fb99.jpg",
+    websiteUrl: "https://termika.orlen.pl/",
+  },
+
+  {
+    id: "c3",
+    name: "VALEO AUTOSYSTEMY SP. Z O.O.",
+    logoUrl: "https://www.valeo.com/wp-content/uploads/2022/12/valeo-logo-rvb_600x327_acf_cropped.png",
+    websiteUrl: "https://www.valeo.com/pl/polska/",
+  },
+
+  // {
+  //   id: "c4",
+  //   name: "SUPERTEAM SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ",
+  //   logoUrl: "https://media.licdn.com/dms/image/v2/D4E0BAQG_gwy-E8l5Dw/company-logo_200_200/company-logo_200_200/0/1735833151283/superteam_pl_logo?e=2147483647&v=beta&t=IECw8DDrT43Xrr5hM2htb5qkwUycns1HGUBiwN9YaDQ",
+  //   websiteUrl: "https://superteam.fun/",
+  // },
+
+  {
+    id: "c5",
+    name: "Invest Komfort Spółka Akcyjna Sp. K.",
+    logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAm2pMk037gymtnIQ44JUcoZvaL1i_I6x7zg&s",
+    websiteUrl: "https://investkomfort.pl/pl",
+  },
+
+  {
+    id: "c6",
+    name: "UTC AEROSPACE SYSTEMS WROCŁAW SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ",
+    logoUrl: "https://dlapilota.pl/sites/default/files/styles/article_popup/public/dlapilota/images/utc.jpg?itok=GBLBCWRL",
+    websiteUrl: "https://www.collinsaerospace.com/who-we-are/about-us/global/europe/poland",
+  },
+  {
+    id: "c7",
+    name: "Emitel S.A.",
+    logoUrl: "https://logotypy.net/wp-content/uploads/2023/09/logo-emitel-300x300.jpg",
+    websiteUrl: "https://emitel.pl/",
+  },
+
+  {
+    id: "c8",
+    name: "Allegro",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Allegro.pl_sklep.svg/2560px-Allegro.pl_sklep.svg.png",
+    websiteUrl: "https://about.allegro.eu/pl",
+  },
+
+  {
+    id: "c9",
+    name: "Philips",
+    logoUrl: "https://www.philips.com/c-dam/corporate/en_AA/about/about-us/homepage/global-homepage-thumbnail.jpg",
+    websiteUrl: "https://www.philips.pl/",
+  },
+];
